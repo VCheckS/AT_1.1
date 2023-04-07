70d2d330680d720f2722b209aace02524545ef06
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-04-07 12:40:25 -0700

    Reformat code junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,34 +9,37 @@ public class CashbackHackServiceTest {
 
     @Test
     public void cashBackTest() {
-        CashbackHackService service= new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int expected = 100;
         int actual = service.remain(amount);
         assertEquals(actual, expected);
 
     }
+
     @Test
     public void testWhenAmountEqualBoundary() {
-        CashbackHackService service= new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int expected = 1000;
         int actual = service.remain(amount);
         assertEquals(actual, expected);
 
     }
+
     @Test
     public void testWhenAmountNull() {
-        CashbackHackService service= new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int expected = 1000;
         int actual = service.remain(amount);
         assertEquals(actual, expected);
 
     }
+
     @Test
     public void testWhenAmountMoreThen10k() {
-        CashbackHackService service= new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 10010;
         int expected = 990;
         int actual = service.remain(amount);
