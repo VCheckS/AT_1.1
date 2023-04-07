fa6c035be7c1b274a253ffad7c00ef1bd8aac558
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-04-07 12:24:01 -0700

    Chenge to testng and add tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testServiceWhenAmount1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testServiceWhenAmount0() {
+        int amount = 0;
+        int expected = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testServiceWhenAmountMoreThen100k() {
+        int amount = 100020;
+        int expected = 980;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+    
+}
