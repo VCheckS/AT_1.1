7a9ae5b92093325ac445256cda0397026c09ec2c
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-04-07 21:44:09 -0700

    Change the test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,7 +9,7 @@ public class CashbackHackServiceTest {
     @Test
     public void testServiceWhenAmount1000() {
         int amount = 1000;
-        int expected = 0;
+        int expected = 1000;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         assertEquals(actual, expected);
