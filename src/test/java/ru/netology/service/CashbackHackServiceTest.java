edb125dbcd5451054ba0a3db0d694f5319cdf7dc
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-04-12 18:12:08 -0700

    Failed test after CI change

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -27,7 +27,7 @@ public class CashbackHackServiceTest {
     @Test
     public void testServiceWhenAmountMoreThen100k() {
         int amount = 100020;
-        int expected = 980;
+        int expected = 98;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         assertEquals(actual, expected);
